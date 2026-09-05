16b65d21861e86ea38b4e7f1ced06213343fdcf2
Erickscpp <neumanerick75@gmail.com>
2026-09-04 21:38:01 -0300

    Corregi ejercicio 2, 9 y 14. Erick Neuman

--- tp2_grupo6/src/ar/edu/unju/escmi/tp2/ejercicio2/Ejercicio2.java
+++ tp2_grupo6/src/ar/edu/unju/escmi/tp2/ejercicio2/Ejercicio2.java
@@ -9,19 +9,21 @@ public class Ejercicio2 {
         String nom3 = "David Palacios";
         String nom4 = "Erick Neuman";
         String nom5 = "Ignacio Jorqui";
+        String nom6 = "Jazmin Figueroa";
 
         int dni1 = 48142750;
         int dni2 = 49109027;
         int dni3 = 48017250;
         int dni4 = 48909125;
         int dni5 = 48142679;
+        int dni6 = 48804234; 
 
         System.out.println(
             nom1 + " - " + dni1 + "\n" +
             nom2 + " - " + dni2 + "\n" +
             nom3 + " - " + dni3 + "\n" +
             nom4 + " - " + dni4 + "\n" +
-            nom5 + " - " + dni5);
-
+            nom5 + " - " + dni5 + "\n" +
+            nom6 + " - " + dni6);
     }
 }
